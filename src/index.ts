1a579ed849a773e103f41ee6a56f71d1239f08f0
joe <joe.chang1014@gmail.com>
2022-02-23 20:32:08 +0800

    feat add socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,55 @@
-import devServer from "./server/dev";
-import prodServer from "./server/prod";
+import devServer from "@/server/dev";
+import prodServer from "@/server/prod";
 import express from "express";
-
-import { name } from "@/utils";
+import { Server } from 'socket.io'
+import http from 'http'
+import UserService from "@/service/UserService";
+import moment from 'moment';
 
 const port = 3000;
 const app = express();
+const server = http.createServer(app)
+const io = new Server(server)
+const userService = new UserService()
+
+// 監測連接
+io.on('connection', (socket) => {
+
+  //發送socket id
+  socket.emit('userID', socket.id)
+  
+  
+  socket.on('join', ({ userName, roomName }: { userName: string, roomName:string }) => {
+    //加入個人資訊
+    const userData = userService.userDataInfoHandler(
+      socket.id,
+      userName,
+      roomName
+    )
+    //加入房間
+    socket.join(userData.roomName)
+    userService.addUser(userData)
+    socket.broadcast.to(userData.roomName).emit('join', `${userName} 加入了 ${roomName} 聊天室`)
+  })
+
+  socket.on('chat', (msg) => {  
+    const time = moment.utc() 
+    const userData = userService.getUser(socket.id)
+    if (userData) {
+      io.to(userData.roomName).emit('chat', { userData, msg, time })
+    }
+  })
+
+  socket.on('disconnect', () => {
+    const userData = userService.getUser(socket.id)
+    const userName = userData?.userName
+    if (userName) {
+      socket.broadcast.to(userData.roomName).emit('leave', `${userData.userName} 離開 ${userData.roomName} 聊天室`)
+    }    
+    userService.removeUser(socket.id)
+  })
+
+})
 
 // 執行npm run dev本地開發 or 執行npm run start部署後啟動線上伺服器
 if (process.env.NODE_ENV === "development") {
@@ -14,8 +58,6 @@ if (process.env.NODE_ENV === "development") {
   prodServer(app);
 }
 
-console.log("server side", name);
-
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`The application is running on port ${port}.`);
 });
