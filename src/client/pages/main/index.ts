d8be22ec1bcc59e9c2cc8e428b864d27b17a3245
joe <joe.chang1014@gmail.com>
2022-02-25 10:41:22 +0800

    feat revise style

--- src/client/pages/main/index.ts
+++ src/client/pages/main/index.ts
@@ -1,4 +1,16 @@
 import "./index.css";
-import { name } from "@/utils";
 
-console.log("client side main page", name);
+
+const nameInput = document.getElementById('nameInput') as HTMLInputElement
+const roomSelect = document.getElementById('roomSelect') as HTMLSelectElement
+const startBtn = document.getElementById('startBtn') as HTMLButtonElement
+
+startBtn.addEventListener('click', () => {
+  const userName = nameInput.value
+  const roomName = roomSelect.value
+  console.log("userName", userName);
+  console.log("roomName", roomName);
+  
+  
+  location.href = `/chatRoom/chatRoom.html?user_name=${userName}&room_name=${roomName}`
+})
